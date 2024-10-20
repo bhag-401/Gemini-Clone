684b4033be1ed94037fbccd2d5dd6d78eb9ce90a
bhag-401 <bhagyashreeray111@gmail.com>
2024-10-20 20:09:48 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Main from "./components/Main/Main"
+import Sidebar from "./components/Sidebar/Sidebar"
+
+let App=()=>{
+  return(
+    <>
+    <Sidebar></Sidebar>
+    <Main></Main>
+    </>
+  )
+}
+export default App
